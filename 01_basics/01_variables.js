6036968ba60a70df589e928b3783c13376c396cd
Aradhya Gupta <152518593+aradhya1012@users.noreply.github.com>
2025-11-08 09:46:05 +0000

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "aaradhya.gupta@gmail.cpm"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+accountEmail = "hc@hc.com"
+accountPassword = "2345"
+accountCity = "pune"
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+/*
+Prefer no to use var bcz of issue in block and functioinal scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treats all JS code as a new version
+//alert(3+3) //works in console and html but for node.js it has different syntax
+let name = "aradhya"
+let age = 18
+let isLoggedIn= false
+let state;
+// number = 2 to powewr 53
+//bigint
+//string = ""
+//boolean= true/false
+//null = standalone value means value aay hi nhi h
+//undefined = assign nhi hua h value
+// symbol = react mai bahut sara component banate to uniquness find karne ke liye use
+//object
+console.log(typeof "aradhya"); // to check datatype can give direct value or variable name
+console.log(typeof age);
+console.log(typeof null);// o/p = object
+console.log(typeof undefined);// o/p=undefined
+
+
+
+
+
+
