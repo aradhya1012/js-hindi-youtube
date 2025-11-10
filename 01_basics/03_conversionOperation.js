634beabb1fd155ce570ce85864f6bcb5f2092f8b
Aradhya Gupta <152518593+aradhya1012@users.noreply.github.com>
2025-11-10 13:44:35 +0000

    Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+let marks = 33
+//console.log(typeof marks);// number
+//console.log(typeof (marks));// number
+
+let temper = "33"
+//console.log(temper);// 33
+//console.log(typeof (temper));//string
+let temperNumber = Number(temper)
+//console.log(typeof (temperNumber));//number
+
+let score = "33abc"
+//console.log(score);// 33abc
+//console.log(typeof score); string
+let valueNumber = Number(score)
+//console.log(typeof (valueNumber)) number
+//console.log((valueNumber)) NaN
+
+let temp = null
+let convTemp = Number(temp)
+//console.log(convTemp); 0
+
+let notgiven = undefined
+let convertValue = Number(notgiven)
+//console.log(convertValue); NaN
+//"33" = 33
+// "33QNH" =NaN
+//TRUE =1 FALSE =0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn); true
+//'' = false 'gyfrtd' =true
+
+
+
+
+
+
+
