c012d6ef0f094667706605b59776e4828a2a62b3
Aradhya Gupta <152518593+aradhya1012@users.noreply.github.com>
2025-11-11 13:09:06 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -31,6 +31,37 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 //console.log(booleanIsLoggedIn); true
 //'' = false 'gyfrtd' =true
 
+// ******************************************************************//
+let value = 3
+let negValue = -value
+//console.log(negValue);
+//console.table([2+2, 2-2, 2*2, 2**3, 2/3, 2%3])
+
+let str1 = "Hello"
+let str2 = " Aradhya"
+let str3 = str1 + str2
+//console.log(str3); Hello Aradhya
+
+//console.log("1" + 2);
+//console.log(2 + "1");
+//console.log("1" + 2 +2);// if 1st is string it will convert all into string if 1st is number then it will perform operation then convert into string
+//console.log(1 + 2 +"2");// not a best practice instead of this use bracket 
+//console.log((3+4) * 5 % 3);
+
+
+//console.log(""); // nothing
+//console.log(+"");//0
+//console.log(+true);//1
+
+
+
+
+
+
+
+
+
+
 
 
 
